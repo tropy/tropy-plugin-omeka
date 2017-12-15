3f5af5a646a8465839400973021dbe80fcf649a3
Kirill Stytsenko <kirstyts@gmail.com>
2017-12-15 17:39:47 +0100

    `src/main` is a script outside of src

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const argv = require('yargs').argv
+const Promise = require('bluebird')
+const path = require('path')
+const readFileAsync = Promise.promisify(require('fs').readFile)
+const { Plugin } = require('../src/plugin')
+
+
+async function readFile(key) {
+  const fileName = path.join(process.cwd(), argv[key])
+  try {
+    return JSON.parse(
+      await readFileAsync(fileName, { encoding: 'utf8' }))
+  } catch (error) {
+    console.error(`Argument --${key} should point to a valid json file`)
+    process.exit()
+  }
+}
+
+async function main() {
+  console.info('Reading items file...')
+
+  const [config, data] = await Promise.all([
+    readFile('config'),
+    readFile('data')
+  ])
+
+  const plugin = new Plugin(config, data)
+  plugin.exec()
+}
+
+main()
